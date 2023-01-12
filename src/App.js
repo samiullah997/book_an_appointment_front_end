92862ec73794846faf99747723bfef8aa960a1d8
samiullah997 <samiullahk997@gmail.com>
2023-01-12 16:42:47 +0500

    Update App.js file and Add AddBike.js layout in the sidebar.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import {
 } from 'react-router-dom';
 import LogIn from './components/authentication/LogIn';
 import SignUp from './components/authentication/SignUp';
+import AddBike from './components/dashboard/AddBike';
 import Home from './components/dashboard/Home';
-// import SideBar from './components/sidebar/SideBar';
+import SideBar from './components/sidebar/sideBar';
 
 const SidebarLayout = () => (
   <>
-    {/* <SideBar /> */}
+    <SideBar />
     <Outlet />
   </>
 );
@@ -22,6 +23,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route element={<SidebarLayout />}>
             <Route path="/user/dashboard" element={<Home />} />
+            <Route path="/user/AddBike" element={<AddBike />} />
           </Route>
         </Routes>
       </BrowserRouter>
