5c3b3c0fafe2c6e86a7f90cf3255ae4adaece5f4
samiullah997 <samiullahk997@gmail.com>
2023-01-13 16:07:32 +0500

    Update Home page

--- src/components/dashboard/Home.js
+++ src/components/dashboard/Home.js
@@ -50,12 +50,15 @@ const Home = () => {
                       $
                       {price}
                     </p>
-                    <Link to={`/user/bikes/${id}`} onClick={() => dispatch(singleBike(id))}>
-                      <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        View Details
-                        <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fillRule="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
-                      </div>
-                    </Link>
+                    <div className="flex flex-row justify-between">
+                      <Link to={`/user/bikes/${id}`} onClick={() => dispatch(singleBike(id))}>
+                        <div className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                          View Details
+                          <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fillRule="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
+                        </div>
+                      </Link>
+                      <div />
+                    </div>
                   </div>
                 </div>
               );
