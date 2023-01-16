e0d8a4a3a17d919501894d858f021cfd2cb11939
Kinyua <alfkinyua@gmail.com>
2023-01-16 11:32:51 +0300

    Add state distructuring

--- src/components/dashboard/Home.js
+++ src/components/dashboard/Home.js
@@ -3,5 +3,23 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { singleBike } from '../../redux/reducer/bikeReducer';
 
-const Home = () => {};
+const Home = () => {
+  const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage] = useState(3);
+  const bikeData = useSelector((state) => state.bikeReducer);
+  const { bikes } = bikeData;
+  const reversed = [...bikes].reverse();
+  if (!reversed) {
+    return <h6 className="text-center">Loading ...</h6>;
+  }
+  const indexOfLastPost = currentPage * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+  const currentPosts = reversed.slice(indexOfFirstPost, indexOfLastPost);
+
+  const prevPage = () => {
+    if (currentPage <= 1) return;
+    setCurrentPage(currentPage - 1);
+  };
+};
 export default Home;
