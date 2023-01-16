ef83778ad3fb623d2eb2ba16f51c0a4ffa914518
Kinyua <alfkinyua@gmail.com>
2023-01-16 11:49:34 +0300

    Add all required imports

--- src/App.js
+++ src/App.js
@@ -1,36 +1,8 @@
-import {
-  BrowserRouter, Outlet, Route, Routes,
-} from 'react-router-dom';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import LogIn from './components/authentication/LogIn';
 import SignUp from './components/authentication/SignUp';
 import AddBike from './components/dashboard/AddBike';
 import DeleteBikes from './components/dashboard/DeleteBikes';
+import Details from './components/dashboard/Details';
 import Home from './components/dashboard/Home';
 import SideBar from './components/sidebar/sideBar';
-
-const SidebarLayout = () => (
-  <>
-    <SideBar />
-    <Outlet />
-  </>
-);
-
-function App() {
-  return (
-    <div className="flex w-screen justify-start h-screen items-center">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LogIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route element={<SidebarLayout />}>
-            <Route path="/user/dashboard" element={<Home />} />
-            <Route path="/user/AddBike" element={<AddBike />} />
-            <Route path="/user/DeleteBikes" element={<DeleteBikes />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
-
-export default App;
