3a8a5b9aa1c3b53f7838f2e134015e044e14a50a
Kinyua <alfkinyua@gmail.com>
2023-01-16 11:37:46 +0300

    Add jsx

--- src/components/dashboard/Home.js
+++ src/components/dashboard/Home.js
@@ -24,5 +24,57 @@ const Home = () => {
     if (currentPage <= 1) return;
     setCurrentPage(currentPage - 1);
   };
+  return (
+    <section>
+      <h2>Hello Welcome to your Dashboard</h2>
+      <h3> </h3>
+      <div>.........................</div>
+      <div>
+        <button type="button" onClick={prevPage}>
+          Prev
+        </button>
+        <div>
+          {currentPosts.map((item) => {
+            const { name, picture, price, id } = item;
+            return (
+              <div key={id}>
+                <img src={picture} alt="" />
+                <div>
+                  <h5>{name}</h5>
+                  <p>${price}</p>
+                  <div>
+                    <Link
+                      to={`/user/Details?id=${id}`}
+                      onClick={() => dispatch(singleBike(id))}
+                    >
+                      <div>
+                        View Details
+                        <svg
+                          aria-hidden="true"
+                          fillRule="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                    </Link>
+                    <div />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <button type="button" onClick={nextPage}>
+          Next
+        </button>
+      </div>
+    </section>
+  );
 };
 export default Home;
