7c8e5d69b269c8ca7bcb967349b14ec47fa58f56
Kinyua <alfkinyua@gmail.com>
2023-01-16 10:24:50 +0300

    Add jsx

--- src/components/dashboard/details.js
+++ src/components/dashboard/details.js
@@ -4,7 +4,23 @@ import { useSelector } from 'react-redux';
 const Details = () => {
   const bikeData = useSelector((state) => state.bikeReducer);
   const { bike } = bikeData;
-  return ();
+  return (
+    <div>
+      <div>
+        <img src={bike.picture} alt="bike picture" />
+      </div>
+      <div>
+        <h1>{bike.name}</h1>
+        <div>
+          <span>Price</span>
+          <span>${bike.price}</span>
+        </div>
+        <h1> {bike.model}</h1>
+        <p> {bike.enginCapacity}</p>
+        <button type="button">Reserve</button>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
