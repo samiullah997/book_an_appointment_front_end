370effbe5de27cd3735cae3ba6160e7dd15a929f
Kinyua <alfkinyua@gmail.com>
2023-01-16 11:50:50 +0300

    Update sidebar component

--- src/App.js
+++ src/App.js
@@ -6,3 +6,11 @@ import DeleteBikes from './components/dashboard/DeleteBikes';
 import Details from './components/dashboard/Details';
 import Home from './components/dashboard/Home';
 import SideBar from './components/sidebar/sideBar';
+
+const SidebarLayout = () => (
+  <>
+    <SideBar />
+    <Outlet />
+  </>
+);
+export SidebarLayout
