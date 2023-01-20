173a4e5088b880375081cf98b60c77e26beb04b5
Kinyua <alfkinyua@gmail.com>
2023-01-20 16:55:46 +0300

    Update test

--- src/components/test/Functional.test.js
+++ src/components/test/Functional.test.js
@@ -1,15 +1,15 @@
-import Random from '../dashboard/Random';
+import Functional from '../dashboard/Functional';
 
 describe('RandomItem', () => {
   test('should get random item from array', () => {
     const array = [1, 2, 3, 4, 5, 6];
-    const item = Random(array);
+    const item = Functional(array);
     expect(array.includes(item)).toBe(true);
   });
 
   test('should return -1 if array is empty', () => {
     const array = [];
-    const item = Random(array);
+    const item = Functional(array);
     expect(item).toBe(-1);
   });
 });
